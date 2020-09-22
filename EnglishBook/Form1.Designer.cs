cc492bcb78cb53983663e53bda2cb2c3bb405d07
Toster124 <45898692+Toster124@users.noreply.github.com>
2020-09-22 20:23:51 +0300

    Add files via upload

---
+++ EnglishBook/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EnglishBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(18, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Главная";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EnglishBook.Properties.Resources.gerb1;
+            this.pictureBox1.Location = new System.Drawing.Point(479, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 465);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(778, 44);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Виртуальный кабинет анлийского языка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 23.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(287, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 588);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Виртуальный кабинет анлийского языка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ EnglishBook/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Security;
+using System.ComponentModel;
+
+namespace EnglishBook
+{
+    public partial class Form1 : Form
+    {
+        private Button[] btnAdd = new Button[10];
+        string dirName = "C://Program Files (x86)/englishbook/";
+        int lol = 0;
+        bool[] dirorfile = new bool[10];
+        string kek;
+        //string[] namedirfiles;
+        string[] namedirfiles = new string[10];
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+            dircheck();
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void but_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (dirorfile[int.Parse(button.Name)])
+            {
+                Process.Start(namedirfiles[int.Parse(button.Name)]);
+            }
+            else
+            {
+                for(int i = 0; i != lol; i++)
+                {
+                    btnAdd[i].Visible = false;
+                }
+                dirName = namedirfiles[int.Parse(button.Name)];
+                lol = 0;
+                dircheck();
+            }
+        }
+
+        void dircheck()
+        {
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                    btnAdd[lol] = new Button();
+                    this.Controls.Add(btnAdd[lol]);
+                    this.btnAdd[lol].Visible = true;
+                    this.btnAdd[lol].Name = lol.ToString();
+                    namedirfiles[lol] = s.ToString();
+                    string name123 = namedirfiles[lol].Substring(36);
+                    label2.Text = "";
+                    while (name123.IndexOf("\\") != -1)
+                    {
+                        label2.Text = name123.Substring(0, name123.IndexOf("\\"));
+                        name123 = name123.Substring(name123.IndexOf("\\") + 1);
+                    }
+                    label2.Update();
+                    this.btnAdd[lol].Text = name123;
+                    this.btnAdd[lol].Location = new System.Drawing.Point(30, 130 + lol * 55);
+                    this.btnAdd[lol].Size = new System.Drawing.Size(425, 50);
+                    this.btnAdd[lol].Click += but_Click;
+                    this.btnAdd[lol].ForeColor = SystemColors.ButtonFace;
+                    this.btnAdd[lol].Font = new Font("Arial", 18, FontStyle.Bold);
+                    dirorfile[lol] = false;
+                    lol++;
+
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                    btnAdd[lol] = new Button();
+                    this.Controls.Add(btnAdd[lol]);
+                    this.btnAdd[lol].Visible = true;
+                    this.btnAdd[lol].Name = lol.ToString();
+                    namedirfiles[lol] = s.ToString();
+                    string name123 = namedirfiles[lol].Substring(36);
+                    label2.Text = "";
+                    while (name123.IndexOf("\\") != -1)
+                    {
+                        label2.Text = name123.Substring(0, name123.IndexOf("\\"));
+                        name123 = name123.Substring(name123.IndexOf("\\")+1);
+                    }
+                    label2.Update();
+                    this.btnAdd[lol].Text = name123;
+                    this.btnAdd[lol].Location = new System.Drawing.Point(30, 130 + lol * 55);
+                    this.btnAdd[lol].Size = new System.Drawing.Size(425, 50);
+                    this.btnAdd[lol].Click += but_Click;
+                    this.btnAdd[lol].ForeColor = SystemColors.ButtonFace;
+                    this.btnAdd[lol].Font = new Font("Arial", 18, FontStyle.Bold);
+                    dirorfile[lol] = true;
+                    lol++;
+                }
+            }
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+                for (int i = 0; i != lol; i++)
+                {
+                    btnAdd[i].Visible = false;
+                }
+                dirName = "C://Program Files (x86)/englishbook/";
+                lol = 0;
+                dircheck();
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
